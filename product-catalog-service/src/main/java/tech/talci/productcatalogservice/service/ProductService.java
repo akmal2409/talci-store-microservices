14655f74ea33a6a05ac332311f8d3fe672e9b86b
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2021-04-18 19:06:15 +0200

    Added binder for productAddedEvent to notify warehouse service

--- product-catalog-service/src/main/java/tech/talci/productcatalogservice/service/ProductService.java
+++ product-catalog-service/src/main/java/tech/talci/productcatalogservice/service/ProductService.java
@@ -1,6 +1,7 @@
 package tech.talci.productcatalogservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 import tech.talci.productcatalogservice.dto.ProductDto;
 import tech.talci.productcatalogservice.dto.mapper.ProductMapper;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 public class ProductService {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final StreamBridge streamBridge;
 
     public List<ProductDto> findAll() {
         return this.productRepository
@@ -27,7 +29,14 @@ public class ProductService {
     public ProductDto save(ProductDto productDto) {
         Product product = productMapper.mapToProduct(productDto);
 
+        // until we get a confirmation from the warehouse service
+        // that the product is available.
+        product.setStatus(Product.Status.OUT_OF_STOCK);
         product = productRepository.save(product);
+
+        // notify warehouse service
+        this.streamBridge
+                .send("productAddedEventSupplier-out-0", product.getSkuCode());
 
         return productMapper.mapToDto(product);
     }
